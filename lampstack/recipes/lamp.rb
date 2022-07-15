aa4cbe18a521792ee80691eaf2babf16e231ce90
Jesmy Kurian <jesmyofficial@gmail.com>
2022-07-15 12:20:29 +0100

    lamp

--- lampstack/recipes/lamp.rb
+++ lampstack/recipes/lamp.rb
@@ -1,4 +1,12 @@
-package %w(lamp-mariadb10.2-php7.2 php7.2 httpd mariadb-server)
+package %w(httpd mariadb-server)
+
+bash 'amazon-extras-install' do
+  user 'root'
+  cwd '/tmp'
+  code <<-EOH
+  amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
+  EOH
+end
 
 service 'httpd' do
   action [:start,:enable]
@@ -23,6 +31,13 @@ bash 'var-www-perms' do
   find /var/www -type d -exec chmod 2775 {} \\;
   find /var/www -type f -exec chmod 0664 {} \\;
   EOH
+end
+
+file '/var/www/html/phpinfo.php' do
+  content '<html><body><h1>My first PHP page</h1><?php echo "Hello World!";?> </body></html>'
+  mode '0755'
+  owner 'ec2-user'
+  group 'apache'
 end
 
 service 'mariadb' do
