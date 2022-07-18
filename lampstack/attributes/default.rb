61353e1aa6aa251aa3d3ff59eff8295d79422a36
Jesmy Kurian <jesmyofficial@gmail.com>
2022-07-18 13:44:50 +0100

    db and app

---
+++ lampstack/attributes/default.rb
@@ -0,0 +1,1 @@
+default['lampstack']['mariadb']['install_sql'] = true
--- lampstack/recipes/lamp.rb
+++ lampstack/recipes/lamp.rb
@@ -42,4 +42,77 @@ end
 
 service 'mariadb' do
   action [:start,:enable]
+  only_if { node['lampstack']['mariadb']['install_sql'] }
 end
+
+sql_pw = ENV['maria_pw']
+
+bash 'mariadb-install' do
+  user 'root'
+  cwd '/tmp'
+  code <<-EOH
+    expect -c "
+    set timeout 10
+    spawn mysql_secure_installation
+    expect \\"Enter current password for root (enter for none):\\"
+    send \\"\r\\"
+    expect \\"Change the root password?\\"
+    send \\"y\r\\"
+    expect \\"New password:\\"
+    send \\"#{sql_pw}\r\\"
+    expect \\"Re-enter new password:\\"
+    send \\"#{sql_pw}\r\\"
+    expect \\"Remove anonymous users?\\"
+    send \\"y\r\\"
+    expect \\"Disallow root login remotely?\\"
+    send \\"y\r\\"
+    expect \\"Remove test database and access to it?\\"
+    send \\"y\r\\"
+    expect \\"Reload privilege tables now?\\"
+    send \\"y\r\\"
+    expect eof"
+  EOH
+  only_if { node['lampstack']['mariadb']['install_sql'] }
+end
+
+ruby_block 'set install_sql' do
+  block do
+    node.set['lampstack']['mariadb']['install_sql'] = false
+    node.save
+  end
+  action :run
+end
+
+package %w(php-mbstring php-xml)
+
+service 'httpd' do
+  action [:restart]
+end
+
+service 'php-fpm' do
+  action [:restart]
+end
+
+remote_file '/var/www/html/phpMyAdmin.tar.gz' do
+  source 'https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-all-languages.tar.gz'
+  owner 'ec2-user'
+  group 'apache'
+  mode '0755'
+  action :create
+end
+
+directory '/var/www/html/phpMyAdmin' do
+  owner 'ec2-user'
+  group 'apache'
+  mode '0755'
+  action :create
+end
+
+archive_file 'phpMyAdmin.tar.gz' do
+  owner 'ec2-user'
+  group 'apache'
+  mode '700'
+  path '/var/www/html/phpMyAdmin.tar.gz'
+  destination '/var/www/html/phpMyAdmin'
+end
+
