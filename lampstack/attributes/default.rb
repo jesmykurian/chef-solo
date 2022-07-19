c9475f4531f7abef50413f897a8ffcbd1173a637
Jesmy Kurian <jesmyofficial@gmail.com>
2022-07-19 12:18:55 +0100

    templates

--- lampstack/attributes/default.rb
+++ lampstack/attributes/default.rb
@@ -1,4 +1,4 @@
 default['lampstack']['install_sql'] = true
 default['lampstack']['user'] = 'ec2-user'
 default['lampstack']['group'] = 'apache'
-node['lampstack']['title'] = 'My lampstack demo application!!'
+default['lampstack']['title'] = 'My lampstack demo application!!'
